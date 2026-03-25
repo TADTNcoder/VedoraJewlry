fbd001b12ac8e38db872adf73ed6ff38fb87d241
Nguyễn Thành Đạt <tadtnwork@gmail.com>
2026-03-25 15:25:27 +0700

    setup cơ bản

---
+++ jewelry-shop-backend/src/main/java/com/jewelry/jewelryshopbackend/JewelryShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jewelry.jewelryshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JewelryShopBackendApplication.class, args);
+    }
+
+}
---
+++ jewelry-shop-backend/src/test/java/com/jewelry/jewelryshopbackend/JewelryShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewelry.jewelryshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
